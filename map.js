6b13151f3e2f5c6783de50d017b1730934660e49
Peter Johnson <insomnia@rcpt.at>
2015-10-08 18:29:45 +0200

    move to ssl tiles

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ var map = L.map('map');
 // var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 // var osm = new L.TileLayer( osmUrl );
 
-L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
   maxZoom: 18
 }).addTo(map);
